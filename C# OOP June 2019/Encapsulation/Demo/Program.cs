c4982f544c3829b05e70e1f0356ac9e7554f064a
BorislavVladimirov <49073192+BorislavVladimirov@users.noreply.github.com>
2019-07-08 00:32:05 +0300

    Fixed previous push. I Think.

---
+++ C# OOP June 2019/Encapsulation/Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3] { 1, 2, 3 };
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
