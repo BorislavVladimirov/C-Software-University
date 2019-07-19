3bbad0b27a9e0049712bdf57d85532a22fba1b0c
BorislavVladimirov <49073192+BorislavVladimirov@users.noreply.github.com>
2019-07-19 22:02:57 +0300

    Added project ReflectionAndAttributes.sln

---
+++ C# OOP June 2019/ReflectionAndAttributes/Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string StealFieldInfo(string investigatedClass, params string[] fieldsName)
+    {
+        Type classType = Type.GetType(investigatedClass);
+
+        FieldInfo[] classFields = classType.GetFields(BindingFlags.Public
+            | BindingFlags.Static
+            | BindingFlags.Instance
+            | BindingFlags.NonPublic);
+
+        StringBuilder sb = new StringBuilder();
+
+        Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+        sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+        foreach (FieldInfo field in classFields.Where(f => fieldsName.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
